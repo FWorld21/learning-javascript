363d726824b8aa3394d686084d75abc8bf4057ce
FWorld21 <FWorld21@protonmail.com>
2021-05-24 12:37:40 +0500

    understand OOP basic

--- 9. OOP/js/script.js
+++ 9. OOP/js/script.js
@@ -1,13 +1,19 @@
 "use strict";
 
-const timeLord = { // Я создал объект.
-    health: 1200, // Я создал свойство и присвоил ему значение.
-    regeneration: function(times) { // Я создал метод этому объекту.
-        this.health -= times * 100; // Я отнимаю из свойства число, переданное в параметры, умноженное на 100.
+let timeLord = {
+    health: 1200,
+    reg: 1,
+    sayHello: function ()  {
+        console.log("Hello world")
     }
-}
+};
 
-let doctor = Object.create(timeLord); // Я создаю ещё один объект на основе этого объекта.
-console.log(doctor.health); // Я вывожу в консоль значение свойства health.
-doctor.regeneration(8); // Я использую метод, передав в параметры 8.
-console.log(doctor.health); // Я вывожу в консоль значение свойтсва health.
+let doctor = {
+    health: 100,
+};
+
+doctor.__proto__ = timeLord;
+
+Object.setPrototypeOf(doctor, timeLord)
+ 
+console.log(doctor.sayHello())
